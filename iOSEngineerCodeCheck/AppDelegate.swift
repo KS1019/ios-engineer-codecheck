217fb9f4bd1204ea37d430475cb3b66b98b91fe2
KOTARO SUTO <ng1a5ss43@gmail.com>
2023-06-02 19:39:54 -0700

    refactor: renames variables, types, and refine comments

--- iOSEngineerCodeCheck/AppDelegate.swift
+++ iOSEngineerCodeCheck/AppDelegate.swift
@@ -1,18 +1,7 @@
-//
-//  AppDelegate.swift
-//  iOSEngineerCodeCheck
-//
-//  Created by 史 翔新 on 2020/04/20.
-//  Copyright © 2020 YUMEMI Inc. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -31,7 +20,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
