3bbfdd95f1e0f154c84d3d4d77ba21dd02446158
KOTARO SUTO <ng1a5ss43@gmail.com>
2023-06-23 06:20:57 +0900

    Add Test

--- iOSEngineerCodeCheckUITests/iOSEngineerCodeCheckUITests.swift
+++ iOSEngineerCodeCheckUITests/iOSEngineerCodeCheckUITests.swift
@@ -3,21 +3,13 @@ import XCTest
 class iOSEngineerCodeCheckUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        continueAfterFailure = false
     }
 
     override func tearDownWithError() throws {}
 
     func testExample() throws {
-        let app = XCUIApplication()
-        app.launch()
     }
 
     func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
     }
 }
